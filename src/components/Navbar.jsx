c8b3bf92fec9326b702fa2589b10a6b72ad8af0a
Lale004 <qocayevalale38@gmail.com>
2023-09-06 19:41:08 +0400

    second

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,38 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
-import { TbHeartFilled} from "react-icons/tb";
-
+import { TbHeartFilled } from "react-icons/tb";
 
 function Navbar() {
-
   return (
     <>
-
       <div className="navbar">
         <div className="container">
           <nav>
-           <div className="nav-img">
-              <Link to="/"> <img src="https://kitpro.site/astron/wp-content/uploads/sites/146/2023/02/LOGO-1.png" alt="" />
-           </Link> </div>
-          
-              <ul className="ul-bir ul-home">
-                <li>
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/blog">Blog</NavLink>
-                </li>
-              </ul>
-    
-              
-                  {" "}
-                  <div className="nav-heart">
-  <NavLink to="/favorites">
-                    <TbHeartFilled  />  </NavLink>
-                  </div>
-              
-
-      
-
-
-     
+            <div className="nav-img">
+              <Link to="/">
+                {" "}
+                <img
+                  src="https://kitpro.site/astron/wp-content/uploads/sites/146/2023/02/LOGO-1.png"
+                  alt=""
+                />
+              </Link>{" "}
+            </div>
+            <ul className="ul-bir ul-home">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/blog">Blog</NavLink>
+              </li>
+            </ul>{" "}
+            <div className="nav-heart">
+              <NavLink to="/favorites">
+                <TbHeartFilled />{" "}
+              </NavLink>
+            </div>
           </nav>
-          </div>
+        </div>
       </div>
     </>
   );
